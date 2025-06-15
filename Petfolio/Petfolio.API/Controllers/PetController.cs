878b84a8d777beac6f9e111ee8a3b068f5840b80
Carlos Henrique <ichfs9108@gmail.com>
2025-06-15 14:36:22 -0300

    endpoint para deletar

--- Petfolio/Petfolio.API/Controllers/PetController.cs
+++ Petfolio/Petfolio.API/Controllers/PetController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Petfolio.Application.UseCases.Pets.Delete;
 using Petfolio.Application.UseCases.Pets.GetAll;
 using Petfolio.Application.UseCases.Pets.GetById;
 using Petfolio.Application.UseCases.Pets.Register;
@@ -60,6 +61,19 @@ namespace Petfolio.API.Controllers
             var response = new GetPetByIdUseCase().Execute(id);
 
             return Ok(response);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(typeof(ResponseErrorsJson), StatusCodes.Status404NotFound)]
+        public IActionResult Delete(int id)
+        {
+            var useCase = new DeletePetByIdUseCase();
+
+            useCase.Execute(id);
+
+            return NoContent();
         }
     }
 }
