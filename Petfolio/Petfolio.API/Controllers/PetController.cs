53785149ceec8fdab7bbe9396d292877aa2778c0
Carlos Henrique <ichfs9108@gmail.com>
2025-06-15 04:13:33 -0300

    endpoint de edicao

--- Petfolio/Petfolio.API/Controllers/PetController.cs
+++ Petfolio/Petfolio.API/Controllers/PetController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Petfolio.Application.UseCases.Pet.Register;
+using Petfolio.Application.UseCases.Pets.Register;
+using Petfolio.Application.UseCases.Pets.Update;
 using Petfolio.Communication.Requests;
 using Petfolio.Communication.Responses;
 
@@ -12,11 +13,23 @@ namespace Petfolio.API.Controllers
 
         [HttpPost]
         [ProducesResponseType(typeof(ResponseRegisterPetJson), StatusCodes.Status201Created)]
-        public IActionResult Register([FromBody] RequestRegisterPetJson request)
+        public IActionResult Register([FromBody] RequestPetJson request)
         {
             var response = new RegisterPetUseCase().Execute(request);
 
             return Created(String.Empty, response);
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult Update([FromRoute] int id, [FromBody] RequestPetJson request)
+        {
+            var useCase = new UpdatePetUseCase(); 
+
+            useCase.Execute(id, request);
+
+            return NoContent();
         }
     }
 }
